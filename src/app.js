d02e5218adc9a0f7726f66aa6e0ad681b87c5ca1
BetaMedina <36626531+BetaMedina@users.noreply.github.com>
2020-04-10 03:39:50 -0300

    Node Challange

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,63 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  if(repositories.length){
+    return response.json(repositories)
+  }
+  return response.status(204).json()
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {...data} = request.body
+  const payload = {id:uuid(),...data,likes:0}
+  repositories.push(payload);
+  return response.status(201).json(payload)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+  const{id} = request.params
+  const{title,url,techs} = request.body
+
+  const index = repositories.findIndex(res=>res.id === id)
+  if(index < 0){
+    return response.status(400).json({
+      err: "Repositorie not found"
+    })
+  }
+  const {likes} = repositories[index]
+  
+  repositories[index] = {id,title,url,techs,likes}
+  return response.json(repositories[index])
+}); 
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const index = repositories.findIndex(res=>res.id === id)
+ 
+  if(index < 0){
+    return response.status(400).json({
+      err: "Repositorie not found"
+    })
+  }
+
+  repositories.splice(index,1);
+  return response.status(204).json({msg:"Deleted with success"})
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const index = repositories.findIndex( res => res.id === id)
+
+  if(index < 0){
+    return response.status(400).json({
+      err: "Repositorie not found"
+    })
+  }
+  const {likes} = repositories[index]
+  repositories[index].likes = likes + 1
+
+  return response.json(repositories[index])
 });
 
 module.exports = app;
